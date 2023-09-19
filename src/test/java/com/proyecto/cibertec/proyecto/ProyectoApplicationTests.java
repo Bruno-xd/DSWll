14969cc10bf1cc696aa06628b82435f82acc9866
Bruno-xd <gonzalesramirezbruno@gmail.com>
2023-09-19 18:56:24 -0500

    first commit

---
+++ src/test/java/com/proyecto/cibertec/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.cibertec.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
